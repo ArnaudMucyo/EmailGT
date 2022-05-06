28b477e85e24aac64a2779362694894b1d6479df
Arnaud Mucyo <arnaud.mucyo@gtbank.local>
2022-05-06 15:22:22 +0200

    Initial commit

---
+++ src/main/java/com/example/EmailerGT/EmailerGtApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmailerGT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailerGtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailerGtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EmailerGT/EmailerGtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmailerGT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailerGtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
